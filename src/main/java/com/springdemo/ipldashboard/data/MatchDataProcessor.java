9d960bd5d3b2dfba84dba14a90b7460b357ca7b7
Akash Singh <akashsingh.work@outlook.com>
2021-04-19 18:11:26 +0530

    Initial commit

---
+++ src/main/java/com/springdemo/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,40 @@
+package com.springdemo.ipldashboard.data;
+
+
+import java.time.LocalDate;
+
+import com.springdemo.model.Match;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+  private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+  @Override
+  public Match process(final MatchInput matchInput) throws Exception {
+
+        Match match = new Match();
+
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+        match.setTeam1(matchInput.getTeam1());
+        match.setTeam2(matchInput.getTeam2());
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setMatchWinner(matchInput.getWinner());
+        match.setResult(matchInput.getResult());
+        match.setResult(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return match; 
+  }
+
+}
